6ab75bf2c4d6fa95203003711763a85b9f4c5000
Devin Smith <github@arzynik.com>
2015-11-02 13:10:52 -0800

    initial

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM php:7.0-fpm
+
+ENV DOCKER=1
+
+EXPOSE 80
+
+RUN apt-get update && apt-get install -y \
+        libmcrypt-dev \
+        php-pear \
+        curl \
+        git \
+		nginx \
+    && docker-php-ext-install iconv mcrypt \
+	&& docker-php-ext-install pdo pdo_mysql
+
+ADD nginx.conf /etc/nginx/sites-available/default
+RUN echo "cgi.fix_pathinfo = 0;" >> /usr/local/etc/php/conf.d/fix_pathinfo.ini
+RUN echo " \n\
+listen = 127.0.0.1:9000" >> /usr/local/etc/php-fpm.conf
+
+ADD run.sh /run.sh
+
+RUN rm -fr /app
+RUN php -v
+
+ONBUILD ADD . /app
+
+ENTRYPOINT ["/run.sh"]
+CMD nginx && php-fpm
---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ "$#" -eq 0 ]; then
+	echo "No command/process type specified"
+        exit 1
+fi
+
+if [ ! -d "/app" ]; then
+	if [ -n "$GIT_REPO" ]; then
+		if [ -n "$GIT_BRANCH" ]; then
+			git clone -B "$GIT_BRANCH" "$GIT_REPO" /app
+		else
+			echo "No \$GIT_BRANCH environment variable defined, assuming MASTER"
+			git clone "$GIT_REPO" /app
+		fi
+		if [ -n "$GIT_TAG" ]; then
+			git --git-dir /app/.git checkout tags/"$GIT_TAG"
+		elif [ -n "$GIT_VERSION" ]; then
+			git --git-dir /app/.git checkout "$GIT_VERSION"
+		fi
+		/build/builder
+	else
+		echo "No \$GIT_REPO environment variable defined"
+		exit 1
+	fi
+fi
+
+if [ "$1" == "/start" ]; then
+	exec $@
+else
+	exec /exec $@
+fi
