6008d6b91d9bdbec2adecdf21120112a9b7b7b26
Devin Smith <github@arzynik.com>
2015-11-03 11:48:16 -0800

    updated for faster builds

--- run.sh
+++ run.sh
@@ -7,15 +7,10 @@ fi
 if [ ! -d "/app" ]; then
 	if [ -n "$GIT_REPO" ]; then
 		if [ -n "$GIT_BRANCH" ]; then
-			git clone -B "$GIT_BRANCH" "$GIT_REPO" /app
+			git clone --single-branch --depth 1 -b "$GIT_BRANCH" "$GIT_REPO" /app
 		else
 			echo "No \$GIT_BRANCH environment variable defined, assuming MASTER"
-			git clone "$GIT_REPO" /app
-		fi
-		if [ -n "$GIT_TAG" ]; then
-			git --git-dir /app/.git checkout tags/"$GIT_TAG"
-		elif [ -n "$GIT_VERSION" ]; then
-			git --git-dir /app/.git checkout "$GIT_VERSION"
+			git clone --single-branch --depth 1 "$GIT_REPO" /app
 		fi
 	else
 		echo "No \$GIT_REPO environment variable defined"
