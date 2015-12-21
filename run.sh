4b5eb14f2a90aac10f036973067f1f7c7e0a6f2a
Devin Smith <github@arzynik.com>
2015-12-20 21:10:28 -0800

    remove app if it exists

--- run.sh
+++ run.sh
@@ -15,27 +15,29 @@ if [[ $TUTUM_TRIGGER_BODY =~ ^GIT_VERSION:.*$ ]]; then
 fi
 
 if [ ! -d "/app" ]; then
-	if [ -n "$GIT_REPO" ]; then
-		if [ -n "$GIT_VERSION" ]; then
-			git clone "$GIT_REPO" /app
-			git --git-dir /app/.git fetch
-			git --git-dir /app/.git checkout $GIT_VERSION
-		else
-			if [ -n "$GIT_BRANCH" ]; then
-				git clone --single-branch --depth 1 -b "$GIT_BRANCH" "$GIT_REPO" /app
-			else
-				echo "No \$GIT_BRANCH environment variable defined, assuming MASTER"
-				git clone --single-branch --depth 1 "$GIT_REPO" /app
-			fi
-		fi
-		if [ -f "/app/build.sh" ]; then
-			/app/build.sh
-		fi
+	rm -Rf /app
+fi
 
+if [ -n "$GIT_REPO" ]; then
+	if [ -n "$GIT_VERSION" ]; then
+		git clone "$GIT_REPO" /app
+		git --git-dir /app/.git fetch
+		git --git-dir /app/.git checkout $GIT_VERSION
 	else
-		echo "No \$GIT_REPO environment variable defined"
-		exit 1
+		if [ -n "$GIT_BRANCH" ]; then
+			git clone --single-branch --depth 1 -b "$GIT_BRANCH" "$GIT_REPO" /app
+		else
+			echo "No \$GIT_BRANCH environment variable defined, assuming MASTER"
+			git clone --single-branch --depth 1 "$GIT_REPO" /app
+		fi
 	fi
+	if [ -f "/app/build.sh" ]; then
+		/app/build.sh
+	fi
+
+else
+	echo "No \$GIT_REPO environment variable defined"
+	exit 1
 fi
 
 exec "$@"
